c0b0707931f291cbb8d1312647ea94e24885380b
Youssef_Abdo <96637360+yousef21321@users.noreply.github.com>
2023-05-04 10:53:45 +0300

    add new

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -7,7 +7,6 @@ import myImage4 from "./magic_hand_pills (1).svg";
 import myImage5 from "./patient_sitting_with_phone (1)11.svg";
 import myImage6 from "./manufacturer_icon.svg";
 
-
 import Video from "../style/video.mp4";
 <video autoPlay loop muted src={Video} type="video/mp4" />;
 const Body = () => {
@@ -19,30 +18,29 @@ const Body = () => {
           <source src={Video} type="video/mp4" />
         </video>
       </div>
-<br/>
-<br/>
-<br/>
+      <br />
+      <br />
+      <br />
 
-        <div className="content">
-          <section class="home" id="home">
-            <div class="content">
-              <h1>The best health care team</h1>
-              <h3>our heros work 24 hours</h3>
-              <a href="#" class="btn">
-                work with us
-              </a>
-            </div>
-          </section>
-{/* //.......................................................................................................... */}
-     
-      <div id="container">
+      <div className="content">
+        <section class="home" id="home">
+          <div class="content">
+            <h1>The best health care team</h1>
+            <h3>our heros work 24 hours</h3>
+            <a href="#" class="btn">
+              work with us
+            </a>
+          </div>
+        </section>
+        {/* //.......................................................................................................... */}
+
+        <div id="container">
           <div className="">
             <div className="undefined ">
               <div className="_3PFI">
                 <div className="_2qOx">
                   <div className="">
                     <main className="_3dzY ">
-                      
                       <div className="bodyne">
                         <div
                           className="_2I6S _2mN8"
@@ -67,27 +65,25 @@ const Body = () => {
                               <div className="t1vT">
                                 <button
                                   type="button"
-                                  className="
-    
-    _1ipg
-    
-  "
-                                >
-                                  Sign Up
+                                  className="_1ipg"
+                                 
+                                ><a class="btn"  href="/signup">Sign Up</a>
+                                  
                                 </button>
                               </div>
                             </div>
                             <div className="_3XEH">
-                              
-      <img className="IziK" src={myImage4} alt="My Image" />
+                              <img
+                                className="IziK"
+                                src={myImage4}
+                                alt="My Image"
+                              />
                             </div>
                           </div>
                         </div>
                         <div className="_2gch">
                           <div className="_1zqG ">
                             <div className="_2RtO ">
-                             
-                             
                               <div className="_2clk"> </div>
                             </div>
                           </div>
@@ -102,8 +98,6 @@ const Body = () => {
                               </div>
                               <div className="_1zqG xWlm">
                                 <div className="_2RtO _1RRD">
-                                 
-
                                   <div className="_1za2">
                                     <div className="_27f8 _2a-6">
                                       Click the link
@@ -115,7 +109,6 @@ const Body = () => {
                                   </div>
                                 </div>
                                 <div className="_2RtO _1RRD">
-                                  
                                   <div className="_1za2">
                                     <div className="_27f8 _2a-6">
                                       Place your order
@@ -126,7 +119,6 @@ const Body = () => {
                                   </div>
                                 </div>
                                 <div className="_2RtO _1RRD">
-                                 
                                   <div className="_1za2">
                                     <div className="_27f8 _2a-6">
                                       Get your prescription delivered
@@ -138,27 +130,28 @@ const Body = () => {
                                   </div>
                                 </div>
                               </div>
-                              
                             </div>
                             <div className="_3XEH">
-                              
-                                    <img  className="IziK" src={myImage5} alt="My Image" />
-
+                              <img
+                                className="IziK"
+                                src={myImage5}
+                                alt="My Image"
+                              />
                             </div>
                           </div>
                         </div>
-                        
-                                  
-                        
+
                         <div className="AxbY _3_eG">
                           <div
                             className="_2k_k"
                             style={{ backgroundColor: "rgb(255, 240, 240)" }}
                           >
                             <div className="_2sJ7 ">
-                             
-      <img className="_257s" src={myImage6} alt="My Image" />
-
+                              <img
+                                className="_257s"
+                                src={myImage6}
+                                alt="My Image"
+                              />
 
                               <div className="_19f2">
                                 For pharma manufacturers
@@ -181,98 +174,94 @@ const Body = () => {
                         </div>
                       </div>
                     </main>
-                   
-                          </div>
-
-                          </div>
-                          </div>
-                          </div>
-                          </div>
-                          </div>
-                          <section class="precautions" id="precautions">
-        <h1 class="heading">take precautions against covid-19</h1>
-
-        <div class="column">
-          <div class="box-container">
-            <h3 class="title">things you should DO</h3>
-
-            <div class="box">
-              <div class="info">
-                <h3>wash your hand</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-                  odit quo saepe illum tempora corrupti sint distinctio,
-                  perferendis maxime illo!
-                </p>
-              </div>
-            </div>
-
-            <div class="box">
-              <div class="info">
-                <h3>always wear a mask</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-                  odit quo saepe illum tempora corrupti sint distinctio,
-                  perferendis maxime illo!
-                </p>
-              </div>
-            </div>
-
-            <div class="box">
-              <div class="info">
-                <h3>stay home during fever</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-                  odit quo saepe illum tempora corrupti sint distinctio,
-                  perferendis maxime illo!
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="box-container">
-            <h3 class="title">things you should NOT DO</h3>
-
-            <div class="box">
-              <div class="info">
-                <h3>avoid close contact with animals</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-                  odit quo saepe illum tempora corrupti sint distinctio,
-                  perferendis maxime illo!
-                </p>
-              </div>
-            </div>
-
-            <div class="box">
-              <div class="info">
-                <h3>avoid close contact with peoples</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-                  odit quo saepe illum tempora corrupti sint distinctio,
-                  perferendis maxime illo!
-                </p>
-              </div>
-            </div>
-
-            <div class="box">
-              <div class="info">
-                <h3>avoid crowded area</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-                  odit quo saepe illum tempora corrupti sint distinctio,
-                  perferendis maxime illo!
-                </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+        <section class="precautions" id="precautions">
+          <h1 class="heading">take precautions against covid-19</h1>
 
-      <Footer/>
+          <div class="column">
+            <div class="box-container">
+              <h3 class="title">things you should DO</h3>
 
+              <div class="box">
+                <div class="info">
+                  <h3>wash your hand</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Vel odit quo saepe illum tempora corrupti sint distinctio,
+                    perferendis maxime illo!
+                  </p>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="info">
+                  <h3>always wear a mask</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Vel odit quo saepe illum tempora corrupti sint distinctio,
+                    perferendis maxime illo!
+                  </p>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="info">
+                  <h3>stay home during fever</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Vel odit quo saepe illum tempora corrupti sint distinctio,
+                    perferendis maxime illo!
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="box-container">
+              <h3 class="title">things you should NOT DO</h3>
+
+              <div class="box">
+                <div class="info">
+                  <h3>avoid close contact with animals</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Vel odit quo saepe illum tempora corrupti sint distinctio,
+                    perferendis maxime illo!
+                  </p>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="info">
+                  <h3>avoid close contact with peoples</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Vel odit quo saepe illum tempora corrupti sint distinctio,
+                    perferendis maxime illo!
+                  </p>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="info">
+                  <h3>avoid crowded area</h3>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Vel odit quo saepe illum tempora corrupti sint distinctio,
+                    perferendis maxime illo!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <Footer />
       </div>
-
     </>
   );
 };
